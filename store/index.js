133fd6c7f8bd6b03b9a394fcca729d593d51e910
bwmgd <1971138851@qq.com>
2022-01-08 23:28:52 +0800

    api环境测试

--- store/index.js
+++ store/index.js
@@ -113,7 +113,8 @@ export const actions = {
         const token = res.data['access_token']
         console.log('the_token', token)
         const req_urls = '/api/user/simplelist?' +
-          'access_token=' + token.text + '&department_id=1&fetch_child=1'
+          'access_token=' + token + '&department_id=1&fetch_child=1'
+        console.log(req_urls)
         return axios.post(req_urls)
       }
     )
